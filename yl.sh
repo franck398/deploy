a9aac439bafbbad0de321ccfaea9c4a964988d36
TONFACK ALAIN <francktonfack167@gmail.com>
2025-07-31 12:57:27 +0100

    deploy update

--- yl.sh
+++ yl.sh
@@ -3,9 +3,9 @@
 echo "while loop starts"
 echo "Performing load testing"
 l=10000 
-while [ $l -le 100000 ]
+while [ $l -le 1000000 ]
 do 
 echo "Tesla Model3 can carry $l kgs"
-l=`expr $l + 10000`
+l=`expr $l + 100000`
 done  
 echo "while loop ends"
